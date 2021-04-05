9272952026cc2ef5f4cc0ae4ec83511eb75a5ee4
Shih-Yu Lee <xu3g4m4@gmail.com>
2021-04-05 20:36:37 +0800

    switch to tf1

--- process.py
+++ process.py
@@ -15,9 +15,9 @@ import tensorflow as tf
 from google.protobuf import text_format
 from object_detection.protos import pipeline_pb2
 
-## Config
+# Config
 
-####  rpoject
+# rpoject
 project_dir = "myproject"
 project_name = "Project_od_1"
 dataset_name = "dataset"
@@ -36,11 +36,12 @@ project_folder = os.path.join(project_dir, project_name)
 dataset_path = os.path.join(project_folder, dataset_name)
 label_path = os.path.join(dataset_path, label_name)
 
-output_folder_path =  os.path.join(project_folder, output_folder)
+output_folder_path = os.path.join(project_folder, output_folder)
 if not os.path.exists(output_folder_path):
     os.makedirs(output_folder_path)
-    
-## Create Label Map (pbtxt)
+
+# Create Label Map (pbtxt)
+
 
 def convert_classes(classes, start=1):
     msg = ''
@@ -50,44 +51,48 @@ def convert_classes(classes, start=1):
         msg = msg + " name: '" + name + "'\n}\n\n"
     return msg[:-1]
 
+
 def create_label_pbtxt(label_path, output_folder_path):
     with open(label_path, 'r') as f:
         data_list = f.read().splitlines()
-    
+
     label_pbtxt_path = os.path.join(output_folder_path, "label_map.pbtxt")
     label_map = convert_classes(data_list)
-    with open( label_pbtxt_path, "w") as f:
+    with open(label_pbtxt_path, "w") as f:
         f.write(label_map)
-        f.close()    
+        f.close()
     print("Create 'label_map.pbtxt' Done")
-    
+
+
 create_label_pbtxt(label_path, output_folder_path)
 
-## Creating TFRecord
+# Creating TFRecord
 
 LABEL_MAP_FILE = os.path.join(output_folder_path, "label_map.pbtxt")
 TRAIN_XML_FILE = os.path.join(dataset_path, "Train")
 TRAIN_TF_RECORD_DIR = os.path.join(output_folder_path, "train.record")
 TEST_XML_FILE = os.path.join(dataset_path, "eval")
-TEST_TF_RECORD_DIR = os.path.join(output_folder_path, "test.record") 
+TEST_TF_RECORD_DIR = os.path.join(output_folder_path, "test.record")
 
-tfrecord_train = "python myproject/generate_tfrecord.py -x {} -l {} -o {}".format(TRAIN_XML_FILE, LABEL_MAP_FILE, TRAIN_TF_RECORD_DIR)
-tfrecord_test = "python myproject/generate_tfrecord.py -x {} -l {} -o {}".format(TEST_XML_FILE, LABEL_MAP_FILE, TEST_TF_RECORD_DIR)
+tfrecord_train = "python myproject/generate_tfrecord.py -x {} -l {} -o {}".format(
+    TRAIN_XML_FILE, LABEL_MAP_FILE, TRAIN_TF_RECORD_DIR)
+tfrecord_test = "python myproject/generate_tfrecord.py -x {} -l {} -o {}".format(
+    TEST_XML_FILE, LABEL_MAP_FILE, TEST_TF_RECORD_DIR)
 
 os.system(tfrecord_train)
 os.system(tfrecord_test)
 
-## Create config
+# Create config
 
-pipeline = pipeline_pb2.TrainEvalPipelineConfig()     
-with tf.io.gfile.GFile('myproject/sample_config/faster_rcnn_sample.config', "r") as f:                                                                                                                                                                                                                     
-    proto_str = f.read()                                                                                                                                                                                                                                          
+pipeline = pipeline_pb2.TrainEvalPipelineConfig()
+with tf.io.gfile.GFile('myproject/sample_config/faster_rcnn_sample.config', "r") as f:
+    proto_str = f.read()
     text_format.Merge(proto_str, pipeline)
-    
+
 feature_extractor_dict = {
-    "resnet50" : "faster_rcnn_resnet50_keras",
-    "resnet101" : "faster_rcnn_resnet101_keras",
-    "inception_resnet_v2" : "faster_rcnn_inception_resnet_v2_keras"
+    "resnet50": "faster_rcnn_resnet50",
+    "resnet101": "faster_rcnn_resnet101",
+    "inception_resnet_v2": "faster_rcnn_inception_v2"
 }
 
 feature_extractor_dict = feature_extractor_dict[NN_architecture]
@@ -106,17 +111,16 @@ pipeline.train_config.batch_size = batch_size
 
 config_output = os.path.join(output_folder_path, "pipeline.config")
 
-config_text = text_format.MessageToString(pipeline)                                                                                                                                                                                                        
-with tf.io.gfile.GFile(config_output, "wb") as f:                                                                                                                                                                                                                       
-        f.write(config_text)
-        
+config_text = text_format.MessageToString(pipeline)
+with tf.io.gfile.GFile(config_output, "wb") as f:
+    f.write(config_text)
+
 MODEL_DIR = os.path.join(output_folder_path, "train")
 train_script = """
-python object_detection/model_main_tf2.py \
+python model_main.py \
   --model_dir={} \
   --pipeline_config_path={} \
-  --alsologtostderr
-  --checkpoint_dir={} 
-""".format(MODEL_DIR, config_output, MODEL_DIR)
+  > training.log 2>&1
+""".format(MODEL_DIR, config_output)
 
 os.system(train_script)
